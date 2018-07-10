60368d60f869c61a4dba44dbeb361fbdc455dda7
clare0125 <clare.woozie1@gmail.com>
2018-07-10 18:29:58 +0000

    automatically backed up by learn

--- holiday_greeting.rb
+++ holiday_greeting.rb
@@ -7,9 +7,9 @@ holiday_greeting("Beyonce","Jay-Z", "Fourth of July")
 
 
 def holiday_greeting1(to_person, from_person, topic)
-  to_person=
-  from_person=
-  topic=
+  to_person= "Mom"
+  from_person= "Your Favorite Child"
+  topic= "Mother's Day"
   
   return "#{to_person} #{from_person} #{topic}"
 end
